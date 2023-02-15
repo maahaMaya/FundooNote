71ee9ed6bb0153b7dd776c324663cc1f860c2ad1
SOURAV <devbhsourav@gmail.com>
2023-02-15 09:00:01 +0530

    [sourav]add : SignInPage

--- fundoo-notes/src/SignIn/SignIn.jsx
+++ fundoo-notes/src/SignIn/SignIn.jsx
@@ -1,4 +1,4 @@
-import './Register.css';
+import './SignIn.css';
 function RegisterNewUser() {
     return (
         <>
--- fundoo-notes/src/index.js
+++ fundoo-notes/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import RegisterNewUser from './Register/Register';
+import RegisterNewUser from './SignIn/SignIn';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
